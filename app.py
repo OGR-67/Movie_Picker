44062d80e62362cbd4705fc7e6530578d1056a0f
Olivier Roos <roosolivier@gmail.com>
2023-10-19 10:39:24 +0200

    feat: 404 and 500 page style + general 500 error handler in app.py

--- app.py
+++ app.py
@@ -36,6 +36,11 @@ def page_not_found(e: HTTPException) -> tuple[str, int]:
     return render_template('404.html'), 404
 
 
+@app.errorhandler(500)
+def internal_server_error(e: HTTPException) -> tuple[str, int]:
+    return render_template('500.html'), 500
+
+
 __all__ = ["app"]
 
 if __name__ == "__main__":
